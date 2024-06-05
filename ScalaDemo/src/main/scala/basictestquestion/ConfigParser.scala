1bcdb6495cb844d448f65211ac5f4b8993055b8b
ReGinWZY <1583952973@qq.com>
2024-06-05 17:07:05 +0800

    2024_training

--- ScalaDemo/src/main/scala/basictestquestion/ConfigParser.scala
+++ ScalaDemo/src/main/scala/basictestquestion/ConfigParser.scala
@@ -1,31 +1,40 @@
 package basictestquestion
 
 import scala.util.matching.Regex
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object ConfigParser {
 
   // 定义配置项的数据结构
   case class SwitchConfig(
                            name: String, // 开关的名称
-                           depList: List[Int], // 依赖的模块 id 列表. 如果为空，需要报错
-                           metaInfo: Map[String, String] = Map(), // 一些元数据。默认为空 Map
-                           enabled: Boolean = true // 是否激活. 默认为 true
+                           properties: Map[String, Any] = Map() // 配置内容
                          )
 
   // 定义解析结果的数据结构，包含解析后的数据或错误信息
   case class Result(
-                     data: Option[Map[String, SwitchConfig]],  // 解析后的数据
+                     data: Option[Map[String, SwitchConfig]], // 解析后的数据
                      error: Option[String] // 第一个错误的信息
                    )
+
+  def stringify(configMap: Map[String, SwitchConfig]): String = {
+    configMap.map { case (name, config) =>
+      config.properties.map {
+        case (key, value: List[_]) => s"$name.$key = [${value.mkString(", ")}]"
+        case (key, value) => s"$name.$key = $value"
+      }.mkString("\n")
+    }.mkString("\n")
+  }
 
   def toPrettyString(result: Result): String = {
     result match {
       case Result(Some(data), None) =>
         val switches = data.map { case (name, config) =>
           s"Switch: $name\n" +
-            s"  Enabled: ${config.enabled}\n" +
-            s"  DepList: ${config.depList.mkString("[", ", ", "]")}\n" +
-            s"  MetaInfo: ${config.metaInfo.map { case (k, v) => s"$k: $v" }.mkString("{", ", ", "}")}\n"
+            config.properties.map { case (k, v) =>
+              s"  $k: $v"
+            }.mkString("\n")
         }.mkString("\n")
         s"Configuration:\n$switches"
       case Result(None, Some(error)) =>
@@ -41,66 +50,68 @@ class ConfigParser {
 
   import ConfigParser._
 
-  def parse(configStr: String): Result = {
+  // 匹配配置项的名称和属性
+  private val namePattern: Regex = "([a-zA-Z][a-zA-Z0-9_]*[a-zA-Z0-9])\\.([a-zA-Z0-9_.]+)\\s*=\\s*(.+)".r
+  // 匹配依赖列表的格式
+  private val listPattern: Regex = "\\[(\\s*\\d+(\\s*,\\s*\\d+)*)]".r
+  // 匹配布尔值
+  private val booleanPattern: Regex = "(true|false)".r
+
+  private val configMap = scala.collection.mutable.Map[String, SwitchConfig]()
+  private var error: Option[String] = None
+
+  private def parse(configStr: String): Result = {
     val lines = configStr.split("\r?\n").filter(_.trim.nonEmpty) // 将配置字符串按行分割并过滤掉空行
-    val configMap = scala.collection.mutable.Map[String, SwitchConfig]() // 用于存储解析后的配置数据
-
-    // 匹配配置项的名称和属性
-    val namePattern: Regex = "([a-zA-Z][a-zA-Z0-9_]*[a-zA-Z0-9])\\.([a-zA-Z0-9_.]+)\\s*=\\s*(.+)".r
-    // 匹配依赖列表的格式
-    val listPattern: Regex = "\\[(\\s*\\d+(\\s*,\\s*\\d+)*)]".r
-    // 匹配布尔值
-    val booleanPattern: Regex = "(true|false)".r
-
-    var error: Option[String] = None
-
     for (line <- lines if error.isEmpty) {
-      line match {
-        case namePattern(switch, property, value) =>
-          if (!configMap.contains(switch)) {
-            configMap(switch) = SwitchConfig(name = switch, depList = List())
-          }
-
-          property match {
-            case "enabled" =>
-              value.trim match {
-                case booleanPattern(b) =>
-                  configMap(switch) = configMap(switch).copy(enabled = b.toBoolean)
-                case _ =>
-                  error = Some(s"$switch.enabled: Invalid boolean value")
-              }
-
-            case "depList" =>
-              value.trim match {
-                case listPattern(list, _) =>
-                  val depList = list.split(",").map(_.trim.toInt).toList
-                  if (depList.nonEmpty) {
-                    configMap(switch) = configMap(switch).copy(depList = depList)
-                  } else {
-                    error = Some(s"$switch.depList: List cannot be empty")
-                  }
-                case _ =>
-                  error = Some(s"$switch.depList: Invalid list format")
-              }
-
-            case metaInfo if metaInfo.startsWith("metaInfo.") =>
-              val key = metaInfo.stripPrefix("metaInfo.")
-              val metaInfoMap = configMap(switch).metaInfo + (key -> value.trim)
-              configMap(switch) = configMap(switch).copy(metaInfo = metaInfoMap)
-
-            case _ =>
-              error = Some(s"$switch.$property: Unknown property")
-          }
-
-        case _ =>
-          error = Some(s"Invalid format: $line")
-      }
+      parseLine(line)
     }
 
+    getResult()
+  }
+
+  def parseLine(line: String): Unit = {
+    line match {
+      case namePattern(switch, property, value) =>
+        if (!configMap.contains(switch)) {
+          configMap(switch) = SwitchConfig(name = switch)
+        }
+
+        val currentConfig = configMap(switch)
+        val parsedValue = parseValue(value.trim)
+        val updatedProperties = currentConfig.properties + (property -> parsedValue)
+        configMap(switch) = currentConfig.copy(properties = updatedProperties)
+
+      case _ =>
+        error = Some(s"Invalid format: $line")
+    }
+  }
+
+  private def parseValue(value: String): Any = {
+    value match {
+      case booleanPattern(b) => b.toBoolean
+      case listPattern(list, _) => list.split(",").map(_.trim).filter(_.nonEmpty).map(_.toInt).toList
+      case _ if value.forall(_.isDigit) => value.toInt
+      case _ => value
+    }
+  }
+
+  def getResult(): Result = {
     error match {
       case Some(err) => Result(None, Some(err))
       case None => Result(Some(configMap.toMap), None)
     }
+  }
+
+  def parseAll(configStrList: List[String]): List[Result] = {
+    import scala.concurrent.Await
+    import scala.concurrent.duration._
+
+    val futures = configStrList.map(configStr => Future {
+      val parser = new ConfigParser()
+      parser.parse(configStr)
+    })
+
+    Await.result(Future.sequence(futures), 10.seconds)
   }
 
 }
--- ScalaDemo/src/main/scala/basictestquestion/Main.scala
+++ ScalaDemo/src/main/scala/basictestquestion/Main.scala
@@ -1,8 +1,9 @@
 package basictestquestion
 
 object Main extends App {
+
   val configStr: String =
-  """
+    """
 switchA.enabled = true
 switchA.depList = [1, 2, 3]
 switchA.metaInfo.owner = "userA"
@@ -12,10 +13,27 @@ switchB.enabled = false
 switchB.depList = [3, 4, 5]
 switchB.metaInfo.owner = "userB"
 switchB.metaInfo.comment = "hello world"
-  """
+"""
 
+  println("【bonus1：逆向操作、bonus2：状态维护】")
   private val parser = new ConfigParser()
-  private val result: ConfigParser.Result = parser.parse(configStr)
-//  println(result)
-  println(ConfigParser.toPrettyString(result))
+  parser.parseLine("switchA.enabled = true")
+  parser.parseLine("switchA.depList = [1, 2, 3]")
+  parser.parseLine("switchA.metaInfo.owner = \"userA\"")
+  parser.parseLine("switchA.metaInfo.comment = \"hello world\"")
+  parser.parseLine("switchB.enabled = false")
+  parser.parseLine("switchB.depList = [3, 4, 5]")
+  parser.parseLine("switchB.metaInfo.owner = \"userB\"")
+  parser.parseLine("switchB.metaInfo.comment = \"hello world\"")
+
+  val result: ConfigParser.Result = parser.getResult()
+  result.data.foreach(data => println(ConfigParser.stringify(data)))
+
+  println("-----------------------------------------------------------")
+
+  println("【bonus3：多线程】")
+  val configStrList: List[String] = List(configStr)
+  private val resultList: List[ConfigParser.Result] = parser.parseAll(configStrList)
+  resultList.foreach(res => println(ConfigParser.toPrettyString(res)))
+
 }
